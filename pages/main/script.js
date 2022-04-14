a5a7ccc37797d0bb3122efde1327956023cdbff0
flyether <flyether@gmail.com>
2022-04-14 18:22:57 +0300

    feat: add email

---
+++ pages/main/script.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+  
+//  });
+
+ $('.owl-carousel').owlCarousel({
+   loop:true,
+  
+   margin:10,
+   nav:true,
+   responsive:{
+       0:{
+           items:1
+       },
+       768:{
+           items:2
+       },
+       1280:{
+           items:3
+       }
+   }
+})
