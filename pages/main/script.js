cdcc99dc71a25b359f8c62247eb87782b18ddbad
flyether <flyether@gmail.com>
2022-04-16 13:28:05 +0300

    feat: add email

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,26 +1,30 @@
-// $(document).ready(function(){
+// бургер и карусель с параметрами
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+    });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        navText:['',''],
+        dots:false,
+        margin:10,
+        nav:true,
+        items:0,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+             margin:30,
+                items:2
+            },
+            1280:{
+                items:3
+            }
+        }
+     
+     
+     })
   
-//  });
-
- $('.owl-carousel').owlCarousel({
-   loop:true,
-   navText:['',''],
-   dots:false,
-   margin:10,
-   nav:true,
-   items:0,
-   responsive:{
-       0:{
-           items:1
-       },
-       768:{
-        margin:30,
-           items:2
-       },
-       1280:{
-           items:3
-       }
-   }
-
-
-})
+  });
+  
